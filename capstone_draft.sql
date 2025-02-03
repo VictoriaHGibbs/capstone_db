c056be566fc9fe2c5e8acb0c27de836e95e82bdc
Victoria Gibbs <victoriahgibbs@gmail.com>
2025-02-03 15:39:17 -0500

    final working code for database

--- capstone_draft.sql
+++ capstone_draft.sql
@@ -1,59 +1,59 @@
-CREATE DATABASE IF NOT EXISTS grub;
-USE grub;
+CREATE DATABASE IF NOT EXISTS victori1_grub;
+USE victori1_grub;
 
 CREATE TABLE `user` (
-  `user_id` int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `user_id` int UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `username` varchar(20) UNIQUE NOT NULL,
-  `profile_image_url` varchar(255),
+  `profile_image_url` varchar(255) DEFAULT NULL,
   `f_name` varchar(50),
   `l_name` varchar(50),
   `email_address` varchar(100) UNIQUE NOT NULL,
   `password_hash` varbinary(60) NOT NULL,
   `joined_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_role_id` tinyint(1) NOT NULL unsigned,
+  `user_role_id` tinyint(1) NOT NULL,
   `active` boolean NOT NULL DEFAULT 1
 );
 
 CREATE TABLE `role` (
-  `user_role_id` tinyint(1) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `user_role_id` tinyint(1) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `user_role` varchar(10) NOT NULL
 );
 
 CREATE TABLE `recipe` (
-  `recipe_id` int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
+  `recipe_id` int UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `user_id` int UNSIGNED NOT NULL,
   `recipe_title` varchar(100) NOT NULL,
-  `prep_time_minutes` int NOT NULL unsigned,
-  `cook_time_minutes` int NOT NULL unsigned,
+  `prep_time_minutes` int UNSIGNED NOT NULL,
+  `cook_time_minutes` int UNSIGNED NOT NULL,
   `description` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `yield` int NOT NULL unsigned,
-  `yield_measurement_id` tinyint NOT NULL,
-  `servings` smallint NOT NULL unsigned,
-  `visibility_id` tinyint(1) NOT NULL unsigned,
+  `yield` int UNSIGNED NOT NULL,
+  `yield_measurement_id` tinyint UNSIGNED NOT NULL,
+  `servings` smallint UNSIGNED NOT NULL,
+  `visibility_id` tinyint(1) UNSIGNED NOT NULL,
 
-  FULLTEXT (`recipe_title`, `description`);
+  FULLTEXT (`recipe_title`, `description`)
 );
 
 CREATE TABLE `ingredient` (
-  `recipe_id` int NOT NULL,
-  `ingredient_line_item` tinyint NOT NULL AUTO_INCREMENT unsigned,
-  `quantity` int NOT NULL unsigned,
+  `recipe_id` int UNSIGNED NOT NULL,
+  `ingredient_line_item` tinyint UNSIGNED NOT NULL,
+  `quantity` int UNSIGNED NOT NULL,
   `ingredient_name` varchar(50) NOT NULL,
-  `measurement_id` tinyint NOT NULL,
+  `measurement_id` tinyint UNSIGNED NOT NULL,
   `sort_order` tinyint NOT NULL DEFAULT (`ingredient_line_item`),
   CONSTRAINT ingredient_pk
     PRIMARY KEY (`recipe_id`, `ingredient_line_item`)
 );
 
 CREATE TABLE `measurement` (
-  `measurement_id` tinyint UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `measurement_id` tinyint UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `measurement_unit` varchar(255) NOT NULL
 );
 
 CREATE TABLE `direction` (
-  `recipe_id` int NOT NULL,
-  `direction_line_item` tinyint NOT NULL AUTO_INCREMENT unsigned,
+  `recipe_id` int UNSIGNED NOT NULL,
+  `direction_line_item` tinyint UNSIGNED NOT NULL,
   `direction_text` varchar(255) NOT NULL,
   `sort_order` tinyint DEFAULT (`direction_line_item`),
   CONSTRAINT direction_pk
@@ -61,8 +61,8 @@ CREATE TABLE `direction` (
 );
 
 CREATE TABLE `rating` (
-  `recipe_id` int NOT NULL,
-  `rater_user_id` int NOT NULL,
+  `recipe_id` int UNSIGNED NOT NULL,
+  `rater_user_id` int UNSIGNED NOT NULL,
   `rating_level` ENUM ('1', '2', '3', '4', '5') NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT rating_pk
@@ -70,8 +70,8 @@ CREATE TABLE `rating` (
 );
 
 CREATE TABLE `image` (
-  `recipe_id` int NOT NULL,
-  `image_line_item` tinyint NOT NULL AUTO_INCREMENT unsigned,
+  `recipe_id` int UNSIGNED NOT NULL,
+  `image_line_item` tinyint UNSIGNED NOT NULL,
   `image_url` varchar(255) NOT NULL,
   `sort_order` tinyint DEFAULT (`image_line_item`),
   CONSTRAINT image_pk
@@ -79,59 +79,59 @@ CREATE TABLE `image` (
 );
 
 CREATE TABLE `video` (
-  `recipe_id` int NOT NULL,
+  `recipe_id` int UNSIGNED NOT NULL,
   `youtube_url` varchar(125) NOT NULL
 );
 
 CREATE TABLE `recipe_meal_type` (
-  `meal_type_id` tinyint NOT NULL,
-  `recipe_id` int NOT NULL,
+  `meal_type_id` tinyint UNSIGNED NOT NULL,
+  `recipe_id` int UNSIGNED NOT NULL,
   PRIMARY KEY (`recipe_id`, `meal_type_id`)
 );
 
 CREATE TABLE `meal_type` (
-  `meal_type_id` tinyint UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `meal_type_id` tinyint UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `meal_type` varchar(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE `recipe_style` (
-  `style_id` tinyint NOT NULL,
-  `recipe_id` int NOT NULL,
+  `style_id` tinyint UNSIGNED NOT NULL,
+  `recipe_id` int UNSIGNED NOT NULL,
   PRIMARY KEY (`recipe_id`, `style_id`)
 );
 
 CREATE TABLE `style` (
-  `style_id` tinyint UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `style_id` tinyint UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `style_type` varchar(35) UNIQUE NOT NULL
 );
 
 CREATE TABLE `recipe_diet` (
-  `diet_id` tinyint NOT NULL,
-  `recipe_id` int NOT NULL,
+  `diet_id` tinyint UNSIGNED NOT NULL,
+  `recipe_id` int UNSIGNED NOT NULL,
   PRIMARY KEY (`recipe_id`, `diet_id`)
 );
 
 CREATE TABLE `diet` (
-  `diet_id` tinyint UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `diet_id` tinyint UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `diet` varchar(35) UNIQUE NOT NULL
 );
 
 CREATE TABLE `visibility` (
-  `visibility_id` tinyint UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `visibility_id` tinyint UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `visibility_type` varchar(255) NOT NULL
 );
 
 CREATE TABLE `comment` (
-  `commenter_user_id` int NOT NULL,
-  `recipe_id` int NOT NULL,
+  `commenter_user_id` int UNSIGNED NOT NULL,
+  `recipe_id` int UNSIGNED NOT NULL,
   `comment_text` text NOT NULL,
   `comment_left_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`commenter_user_id`, `recipe_id`)
 );
 
 CREATE TABLE `follow` (
-  `following_user_id` int NOT NULL,
-  `followed_user_id` int NOT NULL,
+  `following_user_id` int UNSIGNED NOT NULL,
+  `followed_user_id` int UNSIGNED NOT NULL,
   `followed_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`following_user_id`, `followed_user_id`)
 );
