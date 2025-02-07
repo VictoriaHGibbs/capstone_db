73f58606da235b78d0b611a7d8fa558dd9bb92e2
Victoria Gibbs <victoriahgibbs@gmail.com>
2025-02-07 13:56:13 -0500

    Local version of creation code and values for lookup tables

---
+++ local_creation.sql
@@ -0,0 +1,190 @@
+CREATE DATABASE IF NOT EXISTS grub;
+USE grub;
+
+CREATE TABLE `user` (
+  `user_id` int UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) UNIQUE NOT NULL,
+  `profile_image_url` varchar(255) DEFAULT NULL,
+  `f_name` varchar(50),
+  `l_name` varchar(50),
+  `email_address` varchar(100) UNIQUE NOT NULL,
+  `password_hash` varbinary(60) NOT NULL,
+  `joined_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_role_id` tinyint(1) NOT NULL,
+  `active` boolean NOT NULL DEFAULT 1
+);
+
+CREATE TABLE `role` (
+  `user_role_id` tinyint(1) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `user_role` varchar(10) NOT NULL
+);
+
+CREATE TABLE `recipe` (
+  `recipe_id` int UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `user_id` int UNSIGNED NOT NULL,
+  `recipe_title` varchar(100) NOT NULL,
+  `prep_time_minutes` int UNSIGNED NOT NULL,
+  `cook_time_minutes` int UNSIGNED NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `yield` int UNSIGNED NOT NULL,
+  `yield_measurement_id` tinyint UNSIGNED NOT NULL,
+  `servings` smallint UNSIGNED NOT NULL,
+  `visibility_id` tinyint(1) UNSIGNED NOT NULL,
+
+  FULLTEXT (`recipe_title`, `description`)
+);
+
+CREATE TABLE `ingredient` (
+  `recipe_id` int UNSIGNED NOT NULL,
+  `ingredient_line_item` tinyint UNSIGNED NOT NULL,
+  `quantity` int UNSIGNED NOT NULL,
+  `ingredient_name` varchar(50) NOT NULL,
+  `measurement_id` tinyint UNSIGNED NOT NULL,
+  `sort_order` tinyint NOT NULL DEFAULT (`ingredient_line_item`),
+  CONSTRAINT ingredient_pk
+    PRIMARY KEY (`recipe_id`, `ingredient_line_item`)
+);
+
+CREATE TABLE `measurement` (
+  `measurement_id` tinyint UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `measurement_unit` varchar(255) NOT NULL
+);
+
+CREATE TABLE `direction` (
+  `recipe_id` int UNSIGNED NOT NULL,
+  `direction_line_item` tinyint UNSIGNED NOT NULL,
+  `direction_text` varchar(255) NOT NULL,
+  `sort_order` tinyint DEFAULT (`direction_line_item`),
+  CONSTRAINT direction_pk
+    PRIMARY KEY (`recipe_id`, `direction_line_item`)
+);
+
+CREATE TABLE `rating` (
+  `recipe_id` int UNSIGNED NOT NULL,
+  `rater_user_id` int UNSIGNED NOT NULL,
+  `rating_level` ENUM ('1', '2', '3', '4', '5') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT rating_pk
+    PRIMARY KEY (`recipe_id`, `rater_user_id`)
+);
+
+CREATE TABLE `image` (
+  `recipe_id` int UNSIGNED NOT NULL,
+  `image_line_item` tinyint UNSIGNED NOT NULL,
+  `image_url` varchar(255) NOT NULL,
+  `sort_order` tinyint DEFAULT (`image_line_item`),
+  CONSTRAINT image_pk
+    PRIMARY KEY (`recipe_id`, `image_line_item`)
+);
+
+CREATE TABLE `video` (
+  `recipe_id` int UNSIGNED NOT NULL,
+  `youtube_url` varchar(125) NOT NULL
+);
+
+CREATE TABLE `recipe_meal_type` (
+  `meal_type_id` tinyint UNSIGNED NOT NULL,
+  `recipe_id` int UNSIGNED NOT NULL,
+  PRIMARY KEY (`recipe_id`, `meal_type_id`)
+);
+
+CREATE TABLE `meal_type` (
+  `meal_type_id` tinyint UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `meal_type` varchar(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE `recipe_style` (
+  `style_id` tinyint UNSIGNED NOT NULL,
+  `recipe_id` int UNSIGNED NOT NULL,
+  PRIMARY KEY (`recipe_id`, `style_id`)
+);
+
+CREATE TABLE `style` (
+  `style_id` tinyint UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `style_type` varchar(35) UNIQUE NOT NULL
+);
+
+CREATE TABLE `recipe_diet` (
+  `diet_id` tinyint UNSIGNED NOT NULL,
+  `recipe_id` int UNSIGNED NOT NULL,
+  PRIMARY KEY (`recipe_id`, `diet_id`)
+);
+
+CREATE TABLE `diet` (
+  `diet_id` tinyint UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `diet` varchar(35) UNIQUE NOT NULL
+);
+
+CREATE TABLE `visibility` (
+  `visibility_id` tinyint UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `visibility_type` varchar(255) NOT NULL
+);
+
+CREATE TABLE `comment` (
+  `commenter_user_id` int UNSIGNED NOT NULL,
+  `recipe_id` int UNSIGNED NOT NULL,
+  `comment_text` text NOT NULL,
+  `comment_left_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`commenter_user_id`, `recipe_id`)
+);
+
+CREATE TABLE `follow` (
+  `following_user_id` int UNSIGNED NOT NULL,
+  `followed_user_id` int UNSIGNED NOT NULL,
+  `followed_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`following_user_id`, `followed_user_id`)
+);
+
+
+ALTER TABLE `recipe` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+ALTER TABLE `direction` ADD FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`recipe_id`);
+
+ALTER TABLE `ingredient` ADD FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`recipe_id`);
+
+ALTER TABLE `ingredient` ADD FOREIGN KEY (`measurement_id`) REFERENCES `measurement` (`measurement_id`);
+
+ALTER TABLE `user` ADD FOREIGN KEY (`user_role_id`) REFERENCES `role` (`user_role_id`);
+
+ALTER TABLE `rating` ADD FOREIGN KEY (`rater_user_id`) REFERENCES `user` (`user_id`);
+
+ALTER TABLE `rating` ADD FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`recipe_id`);
+
+ALTER TABLE `image` ADD FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`recipe_id`);
+
+ALTER TABLE `recipe_meal_type` ADD FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`recipe_id`);
+
+ALTER TABLE `recipe_meal_type` ADD FOREIGN KEY (`meal_type_id`) REFERENCES `meal_type` (`meal_type_id`);
+
+ALTER TABLE `recipe_diet` ADD FOREIGN KEY (`diet_id`) REFERENCES `diet` (`diet_id`);
+
+ALTER TABLE `recipe_style` ADD FOREIGN KEY (`style_id`) REFERENCES `style` (`style_id`);
+
+ALTER TABLE `recipe_style` ADD FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`recipe_id`);
+
+ALTER TABLE `recipe_diet` ADD FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`recipe_id`);
+
+ALTER TABLE `recipe` ADD FOREIGN KEY (`yield_measurement_id`) REFERENCES `measurement` (`measurement_id`);
+
+ALTER TABLE `recipe` ADD FOREIGN KEY (`visibility_id`) REFERENCES `visibility` (`visibility_id`);
+
+ALTER TABLE `comment` ADD FOREIGN KEY (`commenter_user_id`) REFERENCES `user` (`user_id`);
+
+ALTER TABLE `comment` ADD FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`recipe_id`);
+
+ALTER TABLE `follow` ADD FOREIGN KEY (`following_user_id`) REFERENCES `user` (`user_id`);
+
+ALTER TABLE `follow` ADD FOREIGN KEY (`followed_user_id`) REFERENCES `user` (`user_id`);
+
+ALTER TABLE `video` ADD FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`recipe_id`);
+
+
+DROP USER IF EXISTS 'grubUser'@'localhost';
+
+CREATE USER grubUser@localhost
+IDENTIFIED BY 'Nomnom123!';
+
+GRANT SELECT, INSERT, DELETE, UPDATE
+ON grub.*
+TO grubUser@localhost;
---
+++ lookuptables.sql
@@ -0,0 +1,80 @@
+INSERT INTO role VALUES
+(1, 'user'),
+(2, 'admin'),
+(3, 'superadmin');
+
+INSERT INTO measurement VALUES
+(1, 'teaspoon'),
+(2, 'tablespoon'),
+(3, 'cup'),
+(4, 'pint'),
+(5, 'quart'),
+(6, 'gallon'),
+(7, 'fluid ounce'),
+(8, 'ounce'),
+(9, 'pound'),
+(10, 'gram'),
+(11, 'kilogram'),
+(12, 'liter'),
+(13, 'milliliter');
+
+INSERT INTO visibility VALUES
+(1, 'private'),
+(2, 'public'),
+(3, 'friendsonly');
+
+INSERT INTO diet VALUES
+(1, 'vegetarian'),
+(2, 'vegan'),
+(3, 'keto'),
+(4, 'low-fat'),
+(5, 'low-sugar'),
+(6, 'low-carb'),
+(7, 'mediterranean'),
+(8, 'heart healthy'),
+(9, 'raw'),
+(10, 'whole-food'),
+(11, 'gluten-free');
+
+INSERT INTO style VALUES
+(1, 'american'),
+(2, 'mexican'),
+(3, 'indian'),
+(4, 'italian'),
+(5, 'french'),
+(6, 'cuban'),
+(7, 'chinese'),
+(8, 'japanese'),
+(9, 'thai'),
+(10, 'greek'),
+(11, 'mediterranean'),
+(12, 'korean'),
+(13, 'vietnamese'),
+(14, 'spanish'),
+(15, 'german'),
+(16, 'caribbean'),
+(17, 'middle eastern'),
+(18, 'african'),
+(19, 'brazilian'),
+(20, 'tex-mex'),
+(21, 'fusion');
+
+
+INSERT INTO meal_type VALUES
+(1, 'breakfast'),
+(2, 'brunch'),
+(3, 'lunch'),
+(4, 'snack'),
+(5, 'appetizer'),
+(6, 'dinner'),
+(7, 'supper'),
+(8, 'dessert'),
+(9, 'side dish'),
+(10, 'beverage'),
+(11, 'condiment'),
+(12, 'soup'),
+(13, 'salad'),
+(14, 'main course'),
+(15, 'late-night snack');
+
+
